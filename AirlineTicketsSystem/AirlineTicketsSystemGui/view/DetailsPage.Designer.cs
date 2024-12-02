842ec5142e0951f548347fe9b650a88cd6650920
4lex16 <93353749+4lex16@users.noreply.github.com>
2024-12-01 20:24:59 -0500

    ATK-06: Implemented LoadData function

--- AirlineTicketsSystem/AirlineTicketsSystemGui/view/DetailsPage.Designer.cs
+++ AirlineTicketsSystem/AirlineTicketsSystemGui/view/DetailsPage.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.DetailsTb = new System.Windows.Forms.TextBox();
-            this.PassengerMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.StaffMenuStrip = new System.Windows.Forms.MenuStrip();
             this.pageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.frenchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.PassengerMenuStrip.SuspendLayout();
+            this.StaffMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // DetailsTb
@@ -49,19 +49,19 @@
             this.DetailsTb.Margin = new System.Windows.Forms.Padding(4);
             this.DetailsTb.Multiline = true;
             this.DetailsTb.Name = "DetailsTb";
+            this.DetailsTb.ReadOnly = true;
             this.DetailsTb.Size = new System.Drawing.Size(363, 267);
             this.DetailsTb.TabIndex = 0;
             this.DetailsTb.WordWrap = false;
             // 
-            // PassengerMenuStrip
+            // StaffMenuStrip
             // 
-            this.PassengerMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.pageToolStripMenuItem, this.languageToolStripMenuItem });
-            this.PassengerMenuStrip.Location = new System.Drawing.Point(0, 0);
-            this.PassengerMenuStrip.Name = "PassengerMenuStrip";
-            this.PassengerMenuStrip.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.PassengerMenuStrip.Size = new System.Drawing.Size(396, 24);
-            this.PassengerMenuStrip.TabIndex = 3;
-            this.PassengerMenuStrip.Text = "menuStrip1";
+            this.StaffMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.pageToolStripMenuItem, this.languageToolStripMenuItem });
+            this.StaffMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.StaffMenuStrip.Name = "StaffMenuStrip";
+            this.StaffMenuStrip.Size = new System.Drawing.Size(396, 24);
+            this.StaffMenuStrip.TabIndex = 4;
+            this.StaffMenuStrip.Text = "menuStrip1";
             // 
             // pageToolStripMenuItem
             // 
@@ -106,29 +106,33 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(396, 315);
-            this.Controls.Add(this.PassengerMenuStrip);
+            this.Controls.Add(this.StaffMenuStrip);
             this.Controls.Add(this.DetailsTb);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(15, 15);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "DetailsPage";
-            this.Text = "DetailsPage";
             this.Load += new System.EventHandler(this.DetailsPage_Load);
-            this.PassengerMenuStrip.ResumeLayout(false);
-            this.PassengerMenuStrip.PerformLayout();
+            this.StaffMenuStrip.ResumeLayout(false);
+            this.StaffMenuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        #endregion
-
-        private System.Windows.Forms.TextBox DetailsTb;
-        private System.Windows.Forms.MenuStrip PassengerMenuStrip;
+        private System.Windows.Forms.MenuStrip StaffMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem pageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem frenchToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+
+        private System.Windows.Forms.TextBox DetailsTb;
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip DetailsMenuStrop;
     }
 }
